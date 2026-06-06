b07eb769e044f589adf8cb9f789f1d98b57631e3
fengkezhi <3040428434@qq.com>
2026-06-06 20:32:12 +0800

    fix: 移除DiagnosisServiceImpl.java BOM头，修复Maven编译错误

--- shusixue-backend/src/main/java/com/shusixue/service/impl/DiagnosisServiceImpl.java
+++ shusixue-backend/src/main/java/com/shusixue/service/impl/DiagnosisServiceImpl.java
@@ -1,4 +1,4 @@
-﻿package com.shusixue.service.impl;
+package com.shusixue.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.shusixue.entity.HomeworkSubmission;
